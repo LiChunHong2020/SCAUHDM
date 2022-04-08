bd93894e0ba524eaef32cda8da57eb12694186f5
LiChunHong2020 <103169848+LiChunHong2020@users.noreply.github.com>
2022-04-08 11:30:48 +0800

    Update efficientdet.py

--- efficientdet.py
+++ efficientdet.py
@@ -21,12 +21,6 @@ def preprocess_input(image):
     image /= std
     return image
 
-#--------------------------------------------#
-#   使用自己训练好的模型预测需要修改3个参数
-#   model_path和classes_path和phi都需要修改！
-#   如果出现shape不匹配，一定要注意
-#   训练时的model_path和classes_path参数的修改
-#--------------------------------------------#
 class EfficientDet(object):
     _defaults = {
         "model_path"    : 'model_data/Epoch92-Total_Loss0.0917-Val_Loss0.0749.pth',
@@ -44,17 +38,13 @@ class EfficientDet(object):
         else:
             return "Unrecognized attribute name '" + n + "'"
 
-    #---------------------------------------------------#
-    #   初始化Efficientdet
-    #---------------------------------------------------#
+
     def __init__(self, **kwargs):
         self.__dict__.update(self._defaults)
         self.class_names = self._get_class()
         self.generate()
         
-    #---------------------------------------------------#
-    #   获得所有的分类
-    #---------------------------------------------------#
+
     def _get_class(self):
         classes_path = os.path.expanduser(self.classes_path)
         with open(classes_path) as f:
@@ -62,18 +52,11 @@ class EfficientDet(object):
         class_names = [c.strip() for c in class_names]
         return class_names
     
-    #---------------------------------------------------#
-    #   载入模型
-    #---------------------------------------------------#
+
     def generate(self):
-        #----------------------------------------#
-        #   创建Efficientdet模型
-        #----------------------------------------#
+
         self.net = EfficientDetBackbone(len(self.class_names),self.phi).eval()
 
-        #----------------------------------------#
-        #   载入权值
-        #----------------------------------------#
         print('Loading weights into state dict...')
         state_dict = torch.load(self.model_path)
         self.net.load_state_dict(state_dict)
@@ -82,9 +65,7 @@ class EfficientDet(object):
             self.net = self.net.cuda()
         print('{} model, anchors, and classes loaded.'.format(self.model_path))
 
-        #----------------------------------------#
-        #   画框设置不同的颜色
-        #----------------------------------------#
+
         hsv_tuples = [(x / len(self.class_names), 1., 1.)
                       for x in range(len(self.class_names))]
         self.colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
@@ -92,19 +73,12 @@ class EfficientDet(object):
             map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)),
                 self.colors))
 
-    #---------------------------------------------------#
-    #   检测图片
-    #---------------------------------------------------#
     def detect_image(self, image):
-        #---------------------------------------------------------#
-        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
-        #---------------------------------------------------------#
+
         image       = image.convert('RGB')
 
         image_shape = np.array(np.shape(image)[0:2])
-        #---------------------------------------------------------#
-        #   给图像增加灰条，实现不失真的resize
-        #---------------------------------------------------------#
+
         crop_img    = np.array(letterbox_image(image, (image_sizes[self.phi], image_sizes[self.phi])))
         photo       = np.array(crop_img, dtype = np.float32)
         photo       = np.transpose(preprocess_input(photo), (2, 0, 1))
@@ -113,39 +87,28 @@ class EfficientDet(object):
             images  = torch.from_numpy(np.asarray([photo]))
             if self.cuda:
                 images = images.cuda()
-            #---------------------------------------------------------#
-            #   传入网络当中进行预测
-            #---------------------------------------------------------#
+
             _, regression, classification, anchors = self.net(images)
             
-            #-----------------------------------------------------------#
-            #   将预测结果进行解码
-            #-----------------------------------------------------------#
             regression = decodebox(regression, anchors, images)
             detection = torch.cat([regression, classification],axis=-1)
             batch_detections = non_max_suppression(detection, len(self.class_names),
                                                     conf_thres=self.confidence,
                                                     nms_thres=self.iou)
-            #--------------------------------------#
-            #   如果没有检测到物体，则返回原图
-            #--------------------------------------#
+
             try:
                 batch_detections = batch_detections[0].cpu().numpy()
             except:
                 return image
                 
-            #-----------------------------------------------------------#
-            #   筛选出其中得分高于confidence的框 
-            #-----------------------------------------------------------#
+
             top_index = batch_detections[:,4] > self.confidence
             top_conf = batch_detections[top_index,4]
             top_label = np.array(batch_detections[top_index,-1], np.int32)
             top_bboxes = np.array(batch_detections[top_index,:4])
             top_xmin, top_ymin, top_xmax, top_ymax = np.expand_dims(top_bboxes[:,0],-1),np.expand_dims(top_bboxes[:,1],-1),np.expand_dims(top_bboxes[:,2],-1),np.expand_dims(top_bboxes[:,3],-1)
 
-            #-----------------------------------------------------------#
-            #   去掉灰条部分
-            #-----------------------------------------------------------#
+
             boxes = efficientdet_correct_boxes(top_ymin,top_xmin,top_ymax,top_xmax,np.array([image_sizes[self.phi],image_sizes[self.phi]]),image_shape)
 
         font = ImageFont.truetype(font='model_data/simhei.ttf',size=np.floor(3e-2 * np.shape(image)[1] + 0.5).astype('int32'))
@@ -167,7 +130,6 @@ class EfficientDet(object):
             bottom = min(np.shape(image)[0], np.floor(bottom + 0.5).astype('int32'))
             right = min(np.shape(image)[1], np.floor(right + 0.5).astype('int32'))
 
-            # 画框框
             label = '{} {:.2f}'.format(predicted_class, score)
             draw = ImageDraw.Draw(image)
             label_size = draw.textsize(label, font)
@@ -192,15 +154,11 @@ class EfficientDet(object):
 
 
     def get_FPS(self, image, test_interval):
-        #---------------------------------------------------------#
-        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
-        #---------------------------------------------------------#
+
         image = image.convert('RGB')
 
         image_shape = np.array(np.shape(image)[0:2])
-        #---------------------------------------------------------#
-        #   给图像增加灰条，实现不失真的resize
-        #---------------------------------------------------------#
+
         crop_img    = np.array(letterbox_image(image, (image_sizes[self.phi], image_sizes[self.phi])))
         photo       = np.array(crop_img, dtype = np.float32)
         photo       = np.transpose(preprocess_input(photo), (2, 0, 1))
