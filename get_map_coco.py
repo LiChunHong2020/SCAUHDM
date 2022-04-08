405242873090e6499a7011391a849a804c693c85
LiChunHong2020 <103169848+LiChunHong2020@users.noreply.github.com>
2022-04-08 11:07:18 +0800

    Update get_map_coco.py

--- get_map_coco.py
+++ get_map_coco.py
@@ -10,25 +10,11 @@ def get_classes(classes_path):
     return class_names, len(class_names)
 
 if __name__ == "__main__":
-    '''
-    Recall和Precision不像AP是一个面积的概念，在门限值不同时，网络的Recall和Precision值是不同的。
-    map计算结果中的Recall和Precision代表的是当预测时，门限置信度为0.5时，所对应的Recall和Precision值。
-    此处获得的./map_out/detection-results/里面的txt的框的数量会比直接predict多一些，这是因为这里的门限低，
-    目的是为了计算不同门限条件下的Recall和Precision值，从而实现map的计算。
-    '''
-    # -------------------------------------------------------#
-    #   此处的classes_path用于指定需要测量VOC_map的类别
-    #   一般情况下与训练和预测所用的classes_path一致即可
-    # -------------------------------------------------------#
+
     classes_path = 'model_data/helmet_detection.txt'
-    # -------------------------------------------------------#
-    #   指向VOC数据集所在的文件夹
-    #   默认指向根目录下的VOC数据集
-    # -------------------------------------------------------#
+
     VOCdevkit_path = 'dataset'
-    # -------------------------------------------------------#
-    #   结果输出的文件夹，默认为map_out
-    # -------------------------------------------------------#
+
     map_out_path = 'input'
 
     image_ids = open(os.path.join(VOCdevkit_path, "SCAUHDM2022/ImageSets/Main/test.txt")).read().strip().split()
