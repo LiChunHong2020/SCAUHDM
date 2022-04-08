f2b5d8edd0334e6d8946c45fd35fcd97e54d9a02
LiChunHong2020 <103169848+LiChunHong2020@users.noreply.github.com>
2022-04-08 11:18:35 +0800

    Update get_index.py

--- dataset/SCAUHDM2022/get_index.py
+++ dataset/SCAUHDM2022/get_index.py
@@ -1,26 +1,11 @@
-#----------------------------------------------------------------------#
-#   验证集的划分在train.py代码里面进行
-#   test.txt和val.txt里面没有内容是正常的。训练不会使用到。
-#----------------------------------------------------------------------#
-'''
-#--------------------------------注意----------------------------------#
-如果在pycharm中运行时提示：
-FileNotFoundError: [WinError 3] 系统找不到指定的路径。: './VOCdevkit/VOC2007/Annotations'
-这是pycharm运行目录的问题，最简单的方法是将该文件复制到根目录后运行。
-可以查询一下相对目录和根目录的概念。在VSCODE中没有这个问题。
-#--------------------------------注意----------------------------------#
-'''
+
 import os
 import random 
 random.seed(0)
 
-xmlfilepath=r'F:/efficientnet-pytorch-master/dataset/SCAUHDM2022/Annotations_test'
-saveBasePath=r"F:/efficientnet-pytorch-master/dataset/SCAUHDM2022/ImageSets/Main"
+xmlfilepath=r'./dataset/SCAUHDM2022/Annotations_test'
+saveBasePath=r"./dataset/SCAUHDM2022/ImageSets/Main"
  
-#----------------------------------------------------------------------#
-#   想要增加测试集修改trainval_percent
-#   train_percent不需要修改
-#----------------------------------------------------------------------#
 trainval_percent= 0
 train_percent=1
 
