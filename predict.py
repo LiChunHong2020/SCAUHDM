01ceae91d6b38d4217e2b706f25589cc569b61ec
LiChunHong2020 <103169848+LiChunHong2020@users.noreply.github.com>
2022-04-08 11:44:38 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,8 +1,3 @@
-#----------------------------------------------------#
-#   对视频中的predict.py进行了修改，
-#   将单张图片预测、摄像头检测和FPS测试功能
-#   整合到了一个py文件中，通过指定mode进行模式的修改。
-#----------------------------------------------------#
 import time
 
 import cv2
@@ -13,37 +8,17 @@ from efficientdet import EfficientDet
 
 if __name__ == "__main__":
     efficientdet = EfficientDet()
-    #-------------------------------------------------------------------------#
-    #   mode用于指定测试的模式：
-    #   'predict'表示单张图片预测
-    #   'video'表示视频检测
-    #   'fps'表示测试fps
-    #-------------------------------------------------------------------------#
+
     mode = "predict"
-    #-------------------------------------------------------------------------#
-    #   video_path用于指定视频的路径，当video_path=0时表示检测摄像头
-    #   video_save_path表示视频保存的路径，当video_save_path=""时表示不保存
-    #   video_fps用于保存的视频的fps
-    #   video_path、video_save_path和video_fps仅在mode='video'时有效
-    #   保存视频时需要ctrl+c退出才会完成完整的保存步骤，不可直接结束程序。
-    #-------------------------------------------------------------------------#
+
     video_path      = 0
     video_save_path = ""
     video_fps       = 25.0
-    dir_origin_path = "VOCdevkit/VOC2007/JPEGImages"
-    dir_save_path   = "F:/img_out_efficientdet/"
+    dir_origin_path = "dataset/SCAUHDM2022/JPEGImages"
+    dir_save_path   = " "
 
     if mode == "predict":
-        '''
-        1、该代码无法直接进行批量预测，如果想要批量预测，可以利用os.listdir()遍历文件夹，利用Image.open打开图片文件进行预测。
-        具体流程可以参考get_dr_txt.py，在get_dr_txt.py即实现了遍历还实现了目标信息的保存。
-        2、如果想要进行检测完的图片的保存，利用r_image.save("img.jpg")即可保存，直接在predict.py里进行修改即可。 
-        3、如果想要获得预测框的坐标，可以进入efficientdet.detect_image函数，在绘图部分读取top，left，bottom，right这四个值。
-        4、如果想要利用预测框截取下目标，可以进入efficientdet.detect_image函数，在绘图部分利用获取到的top，left，bottom，right这四个值
-        在原图上利用矩阵的方式进行截取。
-        5、如果想要在预测图上写额外的字，比如检测到的特定目标的数量，可以进入efficientdet.detect_image函数，在绘图部分对predicted_class进行判断，
-        比如判断if predicted_class == 'car': 即可判断当前目标是否为车，然后记录数量即可。利用draw.text即可写字。
-        '''
+
         while True:
             img = input('Input image filename:')
             try:
@@ -65,15 +40,15 @@ if __name__ == "__main__":
         fps = 0.0
         while(True):
             t1 = time.time()
-            # 读取某一帧
+     
             ref,frame=capture.read()
-            # 格式转变，BGRtoRGB
+       
             frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-            # 转变成Image
+          
             frame = Image.fromarray(np.uint8(frame))
-            # 进行检测
+            
             frame = np.array(efficientdet.detect_image(frame))
-            # RGBtoBGR满足opencv显示格式
+       
             frame = cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
             
             fps  = ( fps + (1./(time.time()-t1)) ) / 2
