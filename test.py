ae2b6dfc491be397697bf5854e3dcab7ad6d8af1
LiChunHong2020 <103169848+LiChunHong2020@users.noreply.github.com>
2022-04-08 11:42:50 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,3 @@
-#--------------------------------------------#
-#   该部分代码只用于看网络结构，并非测试代码
-#   map测试请看get_dr_txt.py、get_gt_txt.py
-#   和get_map.py
-#--------------------------------------------#
 import torch
 
 from nets.efficientdet import EfficientDetBackbone
