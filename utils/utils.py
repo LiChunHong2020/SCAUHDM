032782fb2fef40e80397373b64ba05fd07c04753
LiChunHong2020 <103169848+LiChunHong2020@users.noreply.github.com>
2022-04-08 11:33:51 +0800

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -7,43 +7,24 @@ from torchvision.ops import nms
 def decodebox(regression, anchors, img):
     dtype = regression.dtype
     anchors = anchors.to(dtype)
-    #--------------------------------------#
-    #   计算先验框的中心
-    #--------------------------------------#
+
     y_centers_a = (anchors[..., 0] + anchors[..., 2]) / 2
     x_centers_a = (anchors[..., 1] + anchors[..., 3]) / 2
 
-    #--------------------------------------#
-    #   计算先验框的宽高
-    #--------------------------------------#
     ha = anchors[..., 2] - anchors[..., 0]
     wa = anchors[..., 3] - anchors[..., 1]
 
-    #--------------------------------------#
-    #   计算调整后先验框的宽高
-    #   即计算预测框的宽高
-    #--------------------------------------#
     w = regression[..., 3].exp() * wa
     h = regression[..., 2].exp() * ha
 
-    #--------------------------------------#
-    #   计算调整后先验框的中心
-    #   即计算预测框的中心
-    #--------------------------------------#
     y_centers = regression[..., 0] * ha + y_centers_a
     x_centers = regression[..., 1] * wa + x_centers_a
 
-    #--------------------------------------#
-    #   计算预测框的左上角右下角
-    #--------------------------------------#
     ymin = y_centers - h / 2.
     xmin = x_centers - w / 2.
     ymax = y_centers + h / 2.
     xmax = x_centers + w / 2.
 
-    #--------------------------------------#
-    #   将预测框的结果进行堆叠
-    #--------------------------------------#
     boxes = torch.stack([xmin, ymin, xmax, ymax], dim=2)
 
     _, _, height, width = np.shape(img)
@@ -165,10 +146,9 @@ def non_max_suppression(prediction, num_classes, conf_thres=0.5, nms_thres=0.4):
     output = [None for _ in range(len(prediction))]
     
     for image_i, image_pred in enumerate(prediction):
-        # 获得种类及其置信度
+    
         class_conf, class_pred = torch.max(image_pred[:, 4:], 1, keepdim=True)
 
-        # 利用置信度进行第一轮筛选
         conf_mask = (class_conf >= conf_thres).squeeze()
         image_pred = image_pred[conf_mask]
         class_conf, class_pred = class_conf[conf_mask], class_pred[conf_mask]
@@ -176,22 +156,17 @@ def non_max_suppression(prediction, num_classes, conf_thres=0.5, nms_thres=0.4):
         if not image_pred.size(0):
             continue
 
-        # 获得的内容为(x1, y1, x2, y2, class_conf, class_pred)
         detections = torch.cat((image_pred[:, :4], class_conf.float(), class_pred.float()), 1)
 
-        # 获得种类
         unique_labels = detections[:, -1].cpu().unique()
 
         if prediction.is_cuda:
             unique_labels = unique_labels.cuda()
 
         for c in unique_labels:
-            # 获得某一类初步筛选后全部的预测结果
+    
             detections_class = detections[detections[:, -1] == c]
 
-            #------------------------------------------#
-            #   使用官方自带的非极大抑制会速度更快一些！
-            #------------------------------------------#
             keep = nms(
                 detections_class[:, :4],
                 detections_class[:, 4],
@@ -199,22 +174,6 @@ def non_max_suppression(prediction, num_classes, conf_thres=0.5, nms_thres=0.4):
             )
             max_detections = detections_class[keep]
 
-            # # 按照存在物体的置信度排序
-            # _, conf_sort_index = torch.sort(detections_class[:, 4], descending=True)
-            # detections_class = detections_class[conf_sort_index]
-            # # 进行非极大抑制
-            # max_detections = []
-            # while detections_class.size(0):
-            #     # 取出这一类置信度最高的，一步一步往下判断，判断重合程度是否大于nms_thres，如果是则去除掉
-            #     max_detections.append(detections_class[0].unsqueeze(0))
-            #     if len(detections_class) == 1:
-            #         break
-            #     ious = bbox_iou(max_detections[-1], detections_class[1:])
-            #     detections_class = detections_class[1:][ious < nms_thres]
-            # # 堆叠
-            # max_detections = torch.cat(max_detections).data
-            
-            # Add max detections to outputs
             output[image_i] = max_detections if output[image_i] is None else torch.cat(
                 (output[image_i], max_detections))
 
