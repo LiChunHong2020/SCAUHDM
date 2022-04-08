a18a912f11ba63cb71470e15a55d5fed8238fac8
LiChunHong2020 <103169848+LiChunHong2020@users.noreply.github.com>
2022-04-08 10:59:47 +0800

    Update get_map.py

--- get_map.py
+++ get_map.py
@@ -9,11 +9,7 @@ import math
 
 import numpy as np
 
-'''
-用于计算mAP
-代码克隆自https://github.com/Cartucho/mAP
-如果想要设定mAP0.x，比如计算mAP0.75，可以设定MINOVERLAP = 0.75。
-'''
+
 MINOVERLAP = 0.5
 
 parser = argparse.ArgumentParser()
