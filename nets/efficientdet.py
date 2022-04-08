5a66e14034e0f701cc58d134a82ac618922a896a
LiChunHong2020 <103169848+LiChunHong2020@users.noreply.github.com>
2022-04-08 11:39:42 +0800

    Update efficientdet.py

--- nets/efficientdet.py
+++ nets/efficientdet.py
@@ -14,11 +14,7 @@ from nets.attention import cbam_block, eca_block, se_block
 attention_block = [se_block, cbam_block, eca_block]
 
 
-# ----------------------------------#
-#   Xception中深度可分离卷积
-#   先3x3的深度可分离卷积
-#   再1x1的普通卷积
-# ----------------------------------#
+
 class SiLU(nn.Module):
     @staticmethod
     def forward(x):
@@ -134,7 +130,7 @@ class BiFPN(nn.Module):
 
         self.first_time = first_time
         if self.first_time:
-            # 获取到了efficientnet的最后三层，对其进行通道的下压缩
+   
             self.p5_down_channel = nn.Sequential(
                 Conv2dStaticSamePadding(conv_channels[2], num_channels, 1),
                 nn.BatchNorm2d(num_channels, momentum=0.01, eps=1e-3),
@@ -148,7 +144,6 @@ class BiFPN(nn.Module):
                 nn.BatchNorm2d(num_channels, momentum=0.01, eps=1e-3),
             )
 
-            # 对输入进来的p5进行宽高的下采样
             self.p5_to_p6 = nn.Sequential(
                 Conv2dStaticSamePadding(conv_channels[2], num_channels, 1),
                 nn.BatchNorm2d(num_channels, momentum=0.01, eps=1e-3),
@@ -158,7 +153,7 @@ class BiFPN(nn.Module):
                 MaxPool2dStaticSamePadding(3, 2)
             )
 
-            # BIFPN第一轮的时候，跳线那里并不是同一个in
+
             self.p4_down_channel_2 = nn.Sequential(
                 Conv2dStaticSamePadding(conv_channels[1], num_channels, 1),
                 nn.BatchNorm2d(num_channels, momentum=0.01, eps=1e-3),
@@ -168,7 +163,7 @@ class BiFPN(nn.Module):
                 nn.BatchNorm2d(num_channels, momentum=0.01, eps=1e-3),
             )
 
-        # 简易注意力机制的weights
+
         self.p6_w1 = nn.Parameter(torch.ones(2, dtype=torch.float32), requires_grad=True)
         self.p6_w1_relu = nn.ReLU()
         self.p5_w1 = nn.Parameter(torch.ones(2, dtype=torch.float32), requires_grad=True)
@@ -190,7 +185,7 @@ class BiFPN(nn.Module):
         self.attention = attention
 
     def forward(self, inputs):
-        """ bifpn模块结构示意图
+        """ bifpn
             P7_0 -------------------------> P7_2 -------->
                |-------------|                ↑
                              ↓                |
@@ -213,49 +208,35 @@ class BiFPN(nn.Module):
         return p3_out, p4_out, p5_out, p6_out, p7_out
 
     def _forward_fast_attention(self, inputs):
-        # ------------------------------------------------#
-        #   当phi=1、2、3、4、5的时候使用fast_attention
-        #   获得三个shape的有效特征层
-        #   分别是C3  64, 64, 40
+  
+        #         C3  64, 64, 40
         #         C4  32, 32, 112
         #         C5  16, 16, 320
-        # ------------------------------------------------#
+
         if self.first_time:
-            # ------------------------------------------------------------------------#
-            #   第一次BIFPN需要 下采样 与 调整通道 获得 p3_in p4_in p5_in p6_in p7_in
-            # ------------------------------------------------------------------------#
+
             p3, p4, p5 = inputs  # d1: p3(40,80,80) p4(112,40,40) p5(320,20,20)
-            # -------------------------------------------#
-            #   首先对通道数进行调整
-            #   C3 64, 64, 40 -> 64, 64, 64
-            # -------------------------------------------#
+
             p3_in = self.p3_down_channel(p3)  # d1:torch.Size([2, 88, 80, 80])
 
-            # -------------------------------------------#
-            #   首先对通道数进行调整
             #   C4 32, 32, 112 -> 32, 32, 64
-            #                  -> 32, 32, 64
-            # -------------------------------------------#
+   
+ 
             p4_in_1 = self.p4_down_channel(p4)  # d1:torch.Size([2, 88, 40, 40])
             p4_in_2 = self.p4_down_channel_2(p4)  # d1:torch.Size([2, 88, 40, 40])
 
-            # -------------------------------------------#
-            #   首先对通道数进行调整
+ 
             #   C5 16, 16, 320 -> 16, 16, 64
-            #                  -> 16, 16, 64
-            # -------------------------------------------#
+  
             p5_in_1 = self.p5_down_channel(p5)  # d1:torch.Size([2, 88, 20, 20])
             p5_in_2 = self.p5_down_channel_2(p5)  # d1:torch.Size([2, 88, 20, 20])
 
-            # -------------------------------------------#
-            #   对C5进行下采样，调整通道数与宽高
             #   C5 16, 16, 320 -> 8, 8, 64
-            # -------------------------------------------#
+   
             p6_in = self.p5_to_p6(p5)  # d1:torch.Size([2, 88, 10, 10])
-            # -------------------------------------------#
-            #   对P6_in进行下采样，调整宽高
+
             #   P6_in 8, 8, 64 -> 4, 4, 64
-            # -------------------------------------------#
+
             p7_in = self.p6_to_p7(p6_in)  # d1:torch.Size([2, 88, 5, 5])
 
             # p3_in = self.mhsa_p3(p3_in)
@@ -266,46 +247,37 @@ class BiFPN(nn.Module):
             p6_in = self.mhsa_p6(p6_in)
             p7_in = self.mhsa_p7(p7_in)
 
-
-            # 简单的注意力机制，用于确定更关注p7_in还是p6_in
             p6_w1 = self.p6_w1_relu(self.p6_w1)
             weight = p6_w1 / (torch.sum(p6_w1, dim=0) + self.epsilon)
             p6_td = self.conv6_up(self.swish(weight[0] * p6_in + weight[1] * self.p6_upsample(p7_in)))
 
-            # 简单的注意力机制，用于确定更关注p6_up还是p5_in
             p5_w1 = self.p5_w1_relu(self.p5_w1)
             weight = p5_w1 / (torch.sum(p5_w1, dim=0) + self.epsilon)
             p5_td = self.conv5_up(self.swish(weight[0] * p5_in_1 + weight[1] * self.p5_upsample(p6_td)))
 
-            # 简单的注意力机制，用于确定更关注p5_up还是p4_in
             p4_w1 = self.p4_w1_relu(self.p4_w1)
             weight = p4_w1 / (torch.sum(p4_w1, dim=0) + self.epsilon)
             p4_td = self.conv4_up(self.swish(weight[0] * p4_in_1 + weight[1] * self.p4_upsample(p5_td)))
 
-            # 简单的注意力机制，用于确定更关注p4_up还是p3_in
             p3_w1 = self.p3_w1_relu(self.p3_w1)
             weight = p3_w1 / (torch.sum(p3_w1, dim=0) + self.epsilon)
             p3_out = self.conv3_up(self.swish(weight[0] * p3_in + weight[1] * self.p3_upsample(p4_td)))
 
-            # 简单的注意力机制，用于确定更关注p4_in_2还是p4_up还是p3_out
             p4_w2 = self.p4_w2_relu(self.p4_w2)
             weight = p4_w2 / (torch.sum(p4_w2, dim=0) + self.epsilon)
             p4_out = self.conv4_down(
                 self.swish(weight[0] * p4_in_2 + weight[1] * p4_td + weight[2] * self.p4_downsample(p3_out)))
 
-            # 简单的注意力机制，用于确定更关注p5_in_2还是p5_up还是p4_out
             p5_w2 = self.p5_w2_relu(self.p5_w2)
             weight = p5_w2 / (torch.sum(p5_w2, dim=0) + self.epsilon)
             p5_out = self.conv5_down(
                 self.swish(weight[0] * p5_in_2 + weight[1] * p5_td + weight[2] * self.p5_downsample(p4_out)))
 
-            # 简单的注意力机制，用于确定更关注p6_in还是p6_up还是p5_out
             p6_w2 = self.p6_w2_relu(self.p6_w2)
             weight = p6_w2 / (torch.sum(p6_w2, dim=0) + self.epsilon)
             p6_out = self.conv6_down(
                 self.swish(weight[0] * p6_in + weight[1] * p6_td + weight[2] * self.p6_downsample(p5_out)))
 
-            # 简单的注意力机制，用于确定更关注p7_in还是p7_up还是p6_out
             p7_w2 = self.p7_w2_relu(self.p7_w2)
             weight = p7_w2 / (torch.sum(p7_w2, dim=0) + self.epsilon)
             p7_out = self.conv7_down(self.swish(weight[0] * p7_in + weight[1] * self.p7_downsample(p6_out)))
@@ -320,45 +292,38 @@ class BiFPN(nn.Module):
             p6_in = self.mhsa_p6(p6_in)
             p7_in = self.mhsa_p7(p7_in)
 
-            # 简单的注意力机制，用于确定更关注p7_in还是p6_in
             p6_w1 = self.p6_w1_relu(self.p6_w1)
             weight = p6_w1 / (torch.sum(p6_w1, dim=0) + self.epsilon)
             p6_td = self.conv6_up(self.swish(weight[0] * p6_in + weight[1] * self.p6_upsample(p7_in)))
 
-            # 简单的注意力机制，用于确定更关注p6_up还是p5_in
             p5_w1 = self.p5_w1_relu(self.p5_w1)
             weight = p5_w1 / (torch.sum(p5_w1, dim=0) + self.epsilon)
             p5_td = self.conv5_up(self.swish(weight[0] * p5_in + weight[1] * self.p5_upsample(p6_td)))
 
-            # 简单的注意力机制，用于确定更关注p5_up还是p4_in
+
             p4_w1 = self.p4_w1_relu(self.p4_w1)
             weight = p4_w1 / (torch.sum(p4_w1, dim=0) + self.epsilon)
             p4_td = self.conv4_up(self.swish(weight[0] * p4_in + weight[1] * self.p4_upsample(p5_td)))
 
-            # 简单的注意力机制，用于确定更关注p4_up还是p3_in
             p3_w1 = self.p3_w1_relu(self.p3_w1)
             weight = p3_w1 / (torch.sum(p3_w1, dim=0) + self.epsilon)
             p3_out = self.conv3_up(self.swish(weight[0] * p3_in + weight[1] * self.p3_upsample(p4_td)))
 
-            # 简单的注意力机制，用于确定更关注p4_in还是p4_up还是p3_out
             p4_w2 = self.p4_w2_relu(self.p4_w2)
             weight = p4_w2 / (torch.sum(p4_w2, dim=0) + self.epsilon)
             p4_out = self.conv4_down(
                 self.swish(weight[0] * p4_in + weight[1] * p4_td + weight[2] * self.p4_downsample(p3_out)))
 
-            # 简单的注意力机制，用于确定更关注p5_in还是p5_up还是p4_out
             p5_w2 = self.p5_w2_relu(self.p5_w2)
             weight = p5_w2 / (torch.sum(p5_w2, dim=0) + self.epsilon)
             p5_out = self.conv5_down(
                 self.swish(weight[0] * p5_in + weight[1] * p5_td + weight[2] * self.p5_downsample(p4_out)))
 
-            # 简单的注意力机制，用于确定更关注p6_in还是p6_up还是p5_out
             p6_w2 = self.p6_w2_relu(self.p6_w2)
             weight = p6_w2 / (torch.sum(p6_w2, dim=0) + self.epsilon)
             p6_out = self.conv6_down(
                 self.swish(weight[0] * p6_in + weight[1] * p6_td + weight[2] * self.p6_downsample(p5_out)))
 
-            # 简单的注意力机制，用于确定更关注p7_in还是p7_up还是p6_out
             p7_w2 = self.p7_w2_relu(self.p7_w2)
             weight = p7_w2 / (torch.sum(p7_w2, dim=0) + self.epsilon)
             p7_out = self.conv7_down(self.swish(weight[0] * p7_in + weight[1] * self.p7_downsample(p6_out)))
@@ -366,9 +331,9 @@ class BiFPN(nn.Module):
         return p3_out, p4_out, p5_out, p6_out, p7_out
 
     def _forward(self, inputs):
-        # 当phi=6、7的时候使用_forward
+        # phi=6、7
         if self.first_time:
-            # 第一次BIFPN需要下采样与降通道获得
+ 
             # p3_in p4_in p5_in p6_in p7_in
             p3, p4, p5 = inputs
             p3_in = self.p3_down_channel(p3)
@@ -430,20 +395,18 @@ class BoxNet(nn.Module):
 
         self.conv_list = nn.ModuleList(
             [SeparableConvBlock(in_channels, in_channels, norm=False, activation=False) for i in range(num_layers)])
-        # 每一个有效特征层对应的Batchnor不同
+
         self.bn_list = nn.ModuleList(
             [nn.ModuleList([nn.BatchNorm2d(in_channels, momentum=0.01, eps=1e-3) for i in range(num_layers)]) for j in
              range(5)])
-        # 9
-        # 4 中心，宽高
+
         self.header = SeparableConvBlock(in_channels, num_anchors * 4, norm=False, activation=False)
         self.swish = MemoryEfficientSwish() if not onnx_export else Swish()
 
     def forward(self, inputs):
         feats = []
-        # 对每个特征层循环
+
         for feat, bn_list in zip(inputs, self.bn_list):
-            # 每个特征层需要进行num_layer次卷积+标准化+激活函数
             for i, bn, conv in zip(range(self.num_layers), bn_list, self.conv_list):
                 feat = conv(feat)
                 feat = bn(feat)
@@ -454,7 +417,7 @@ class BoxNet(nn.Module):
             feat = feat.contiguous().view(feat.shape[0], -1, 4)
 
             feats.append(feat)
-        # 进行一个堆叠
+
         feats = torch.cat(feats, dim=1)
 
         return feats
@@ -468,21 +431,20 @@ class ClassNet(nn.Module):
         self.num_layers = num_layers
         self.conv_list = nn.ModuleList(
             [SeparableConvBlock(in_channels, in_channels, norm=False, activation=False) for i in range(num_layers)])
-        # 每一个有效特征层对应的BatchNorm2d不同
+
         self.bn_list = nn.ModuleList(
             [nn.ModuleList([nn.BatchNorm2d(in_channels, momentum=0.01, eps=1e-3) for i in range(num_layers)]) for j in
              range(5)])
-        # num_anchors = 9
-        # num_anchors num_classes
+
         self.header = SeparableConvBlock(in_channels, num_anchors * num_classes, norm=False, activation=False)
         self.swish = MemoryEfficientSwish() if not onnx_export else Swish()
 
     def forward(self, inputs):
         feats = []
-        # 对每个特征层循环
+
         for feat, bn_list in zip(inputs, self.bn_list):
             for i, bn, conv in zip(range(self.num_layers), bn_list, self.conv_list):
-                # 每个特征层需要进行num_layer次卷积+标准化+激活函数
+        
                 feat = conv(feat)
                 feat = bn(feat)
                 feat = self.swish(feat)
@@ -494,9 +456,9 @@ class ClassNet(nn.Module):
             feat = feat.contiguous().view(feat.shape[0], -1, self.num_classes)
 
             feats.append(feat)
-        # 进行一个堆叠
+
         feats = torch.cat(feats, dim=1)
-        # 取sigmoid表示概率
+    
         feats = feats.sigmoid()
 
         return feats
@@ -525,11 +487,7 @@ class EfficientNet(nn.Module):
             if drop_connect_rate:
                 drop_connect_rate *= float(idx) / len(self.model._blocks)
             x = block(x, drop_connect_rate=drop_connect_rate)
-            # ------------------------------------------------------#
-            #   取出对应的特征层，如果某个EffcientBlock的步长为2的话
-            #   意味着它的前一个特征层为有效特征层
-            #   除此之外，最后一个EffcientBlock的输出为有效特征层
-            # ------------------------------------------------------#
+
             if block._depthwise_conv.stride == [2, 2]:
                 feature_maps.append(last_x)
             elif idx == len(self.model._blocks) - 1:
@@ -542,30 +500,18 @@ class EfficientNet(nn.Module):
 class EfficientDetBackbone(nn.Module):
     def __init__(self, num_classes=80, phi=0, load_weights=False, ph=0):
         super(EfficientDetBackbone, self).__init__()
-        # --------------------------------#
-        #   phi指的是efficientdet的版本
-        # --------------------------------#
+
         self.phi = phi
         self.ph = ph
-        # ---------------------------------------------------#
-        #   backbone_phi指的是该efficientdet对应的efficient
-        # ---------------------------------------------------#
+
         self.backbone_phi = [0, 1, 2, 3, 4, 5, 6, 6]
-        # --------------------------------#
-        #   BiFPN所用的通道数a
-        # --------------------------------#
+
         self.fpn_num_filters = [64, 88, 112, 160, 224, 288, 384, 384]
-        # --------------------------------#
-        #   BiFPN的重复次数
-        # --------------------------------#
+
         self.fpn_cell_repeats = [3, 4, 5, 6, 7, 7, 8, 8]
-        # ---------------------------------------------------#
-        #   Effcient Head卷积重复次数
-        # ---------------------------------------------------#
+
         self.box_class_repeats = [3, 3, 3, 4, 4, 4, 5, 5]
-        # ---------------------------------------------------#
-        #   基础的先验框大小
-        # ---------------------------------------------------#
+
         self.anchor_scale = [4., 4., 4., 4., 4., 4., 4., 5.]
 
         num_anchors = 9
@@ -601,15 +547,13 @@ class EfficientDetBackbone(nn.Module):
         # self.mhsa_p6 = BottleneckAttention(dim=112, fmap_size=(12, 12), heads=16)
         # self.mhsa_p7 = BottleneckAttention(dim=112, fmap_size=(6, 6), heads=16)
 
-        # ------------------------------------------------------#
-        #   在经过多次BiFPN模块的堆叠后，我们获得的fpn_features
-        #   假设我们使用的是efficientdet-D0包括五个有效特征层：
+
         #   P3_out      64,64,64
         #   P4_out      32,32,64
         #   P5_out      16,16,64
         #   P6_out      8,8,64
         #   P7_out      4,4,64
-        # ------------------------------------------------------#
+        
         self.bifpn = nn.Sequential(
             *[BiFPN(self.fpn_num_filters[self.phi],
                     conv_channel_coef[phi],
@@ -618,10 +562,7 @@ class EfficientDetBackbone(nn.Module):
               for _ in range(self.fpn_cell_repeats[phi])])
 
         self.num_classes = num_classes
-        # ------------------------------------------------------#
-        #   创建efficient head
-        #   可以将特征层转换成预测结果
-        # ------------------------------------------------------#
+
         self.regressor = BoxNet(in_channels=self.fpn_num_filters[self.phi], num_anchors=num_anchors,
                                 num_layers=self.box_class_repeats[self.phi])
 
@@ -630,12 +571,6 @@ class EfficientDetBackbone(nn.Module):
 
         self.anchors = Anchors(anchor_scale=self.anchor_scale[phi])
 
-        # -------------------------------------------#
-        #   获得三个shape的有效特征层
-        #   分别是C3  64, 64, 40
-        #         C4  32, 32, 112
-        #         C5  16, 16, 320
-        # -------------------------------------------#
         self.backbone_net = EfficientNet(self.backbone_phi[phi], load_weights)
 
     def freeze_bn(self):
