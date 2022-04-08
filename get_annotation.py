63edf2e3ca9b774850e143832429956489a1e9e7
LiChunHong2020 <103169848+LiChunHong2020@users.noreply.github.com>
2022-04-08 10:57:03 +0800

    Add files via upload

---
+++ get_annotation.py
@@ -0,0 +1,42 @@
+#---------------------------------------------#
+#   运行前一定要修改classes
+#   如果生成的2007_train.txt里面没有目标信息
+#   那么就是因为classes没有设定正确
+#---------------------------------------------#
+import xml.etree.ElementTree as ET
+from os import getcwd
+
+sets=[('2022', 'train'), ('2022', 'val'), ('2022', 'test')]
+#-----------------------------------------------------#
+#   这里设定的classes顺序要和model_data里的txt一样
+#-----------------------------------------------------#
+classes = ["helmet", "bicycler","no helmet", "motorcycler"]
+
+
+def convert_annotation(year, image_id, list_file):
+    in_file = open('dataset/SCAUHDM%s/Annotations_test/%s.xml'%(year, image_id), encoding='utf-8')
+    tree=ET.parse(in_file)
+    root = tree.getroot()
+
+    for obj in root.iter('object'):
+        difficult = 0 
+        if obj.find('difficult')!=None:
+            difficult = obj.find('difficult').text
+        cls = obj.find('name').text
+        if cls not in classes or int(difficult)==1:
+            continue
+        cls_id = classes.index(cls)
+        xmlbox = obj.find('bndbox')
+        b = (int(float(xmlbox.find('xmin').text)), int(float(xmlbox.find('ymin').text)), int(float(xmlbox.find('xmax').text)), int(float(xmlbox.find('ymax').text)))
+        list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
+
+wd = getcwd()
+
+for year, image_set in sets:
+    image_ids = open('dataset/SCAUHDM%s/ImageSets/Main/%s.txt'%(year, image_set), encoding='utf-8').read().strip().split()
+    list_file = open('%s_%s.txt'%(year, image_set), 'w', encoding='utf-8')
+    for image_id in image_ids:
+        list_file.write('%s/dataset/SCAUHDM%s/JPEGImages_test/%s.jpg'%(wd, year, image_id))
+        convert_annotation(year, image_id, list_file)
+        list_file.write('\n')
+    list_file.close()
